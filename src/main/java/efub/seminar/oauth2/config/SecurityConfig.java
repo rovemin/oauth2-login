e81bb9582731a5004faff35f3518c871971afa3f
Minseo Cho <rove.minseo@gmail.com>
2023-11-06 19:14:45 +0900

    ✨ initial setting

--- src/main/java/efub/seminar/oauth2/config/SecurityConfig.java
+++ src/main/java/efub/seminar/oauth2/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package efub.seminar.oauth2.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,12 +10,15 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf().disable()       // csrf 보안 설정 사용 X
+                .httpBasic().disable()
+                .cors().and()
+                .csrf().disable()
                 .authorizeRequests()    // 사용자가 보내는 요청에 인증 절차 수행 필요
                 .antMatchers("/**").permitAll()   // 해당 URL은 인증 절차 수행 생략 가능
                 .anyRequest().authenticated()   // 나머지 요청들은 모두 인증 절차 수행 필요
